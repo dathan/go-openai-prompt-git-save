ff958ca1698643974a0a9826f6f37b038ad3a402
Dathan Vance Pattishall <dathanvp@gmail.com>
2023-04-05 09:09:19 -0700

    feat: add a new pkg to move commands into its own domain

--- cmd/chatgptgit/main.go
+++ cmd/chatgptgit/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"os"
-	"strings"
 
+	. "github.com/dathan/go-openai-prompt-git-save/pkg/commands"
 	"github.com/dathan/go-openai-prompt-git-save/pkg/githubstorage"
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
@@ -21,11 +20,6 @@ var chatCmd = &cobra.Command{
 		fmt.Println("Welcome to the OpenAI GPT-4 chatbot. Type '/exit' to quit.")
 		for {
 			fmt.Print("> ")
-			/*
-				fmt.Printf("Using debug mode")
-				input := "What are 5 fun facts about golang"
-			*/
-
 			reader := bufio.NewReader(os.Stdin)
 			input, err := reader.ReadString('\n')
 			if err != nil {
@@ -33,12 +27,11 @@ var chatCmd = &cobra.Command{
 				os.Exit(1)
 			}
 
-			if strings.Trim(input, "\n") == "/exit" {
-				fmt.Println("Thanks for chatting!")
-				os.Exit(0)
-			}
+			c := NewCommand(input)
 
-			resp, err := chatCommand(client, input)
+			c.HandleSlashCommand()
+
+			resp, err := c.HandleChatCommand(client)
 			if err != nil {
 				fmt.Println("Error with GPT4, ", err)
 				os.Exit(1)
@@ -53,23 +46,6 @@ var chatCmd = &cobra.Command{
 		}
 
 	},
-}
-
-func chatCommand(client *openai.Client, content string) (resp openai.ChatCompletionResponse, err error) {
-
-	return client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: content + "\n",
-				},
-			},
-		},
-	)
-
 }
 
 func validateEnvironment() {
---
+++ pkg/commands/commands.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+type Command struct {
+	command string
+}
+
+func NewCommand(input string) *Command {
+	return &Command{
+		command: strings.Trim(input, "\n"),
+	}
+}
+
+func (c *Command) HandleSlashCommand() {
+
+	switch c.command {
+	case "/exit":
+		fmt.Println("Thanks for Chatting")
+		os.Exit(0)
+	}
+
+}
+
+func (c *Command) HandleChatCommand(client *openai.Client) (openai.ChatCompletionResponse, error) {
+
+	return client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: c.command + "\n",
+				},
+			},
+		},
+	)
+
+}
